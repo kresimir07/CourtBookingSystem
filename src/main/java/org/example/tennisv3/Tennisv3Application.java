c78cde47e12f1bd08e50711e76fc501f577428ff
kresimir <kresimir.vulic07@gmail.com>
2024-11-02 11:26:00 +0100

    Initial commit

---
+++ src/main/java/org/example/tennisv3/Tennisv3Application.java
@@ -0,0 +1,13 @@
+package org.example.tennisv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tennisv3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tennisv3Application.class, args);
+    }
+
+}
